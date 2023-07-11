404319c2291688538787a2089e5a97b83509f4c4
AishSunbae <gdori2018@gmail.com>
2023-07-12 00:00:30 +0200

    add prettier

--- url-shortner/src/App.js
+++ url-shortner/src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Form from './components/Form';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Form from "./components/Form";
 
 function App() {
   return (
-<div className="App">
-  <div className="auth-wrapper">
-    <div className="auth-inner">
-      <switch>
-        <Route exact path='/' component={Form} />
-        <Route path='/app' component={Form} />
-      </switch>
+    <div className="App">
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <switch>
+            <Route exact path="/" component={Form} />
+            <Route path="/app" component={Form} />
+          </switch>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 }
 
--- url-shortner/src/index.js
+++ url-shortner/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter} from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { initializeApp } from 'firebase/app';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
@@ -16,7 +16,7 @@ const firebaseConfig = {
   storageBucket: "url-shortner-1c4e1.appspot.com",
   messagingSenderId: "872898950842",
   appId: "1:872898950842:web:1af065f58443a43b64b483",
-  measurementId: "G-DW5DDEKHHB"
+  measurementId: "G-DW5DDEKHHB",
 };
 
 // Initialize Firebase
@@ -24,11 +24,11 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 // Render React
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
